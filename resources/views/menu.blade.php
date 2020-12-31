9ddc8480aa89c4e0d4fa2cfd85ff57cdf55b4306
Leo Leo <xieyueqingxyq@gmail.com>
2020-12-31 15:08:39 -0500

    Add

---
+++ resources/views/menu.blade.php
@@ -0,0 +1,316 @@
+@extends('layouts.menulayout')
+
+@section('content')
+<div class="cntr">
+
+
+            <h4 class=" center">Menu</h4>
+                 
+            <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-3">
+            <div class="card">
+            <div class="card-image">
+                <img src="/img/1.jpg"  alt="" class="responsive-img "/>
+                <span class="card-title">Spicy Crayfish - $45</span>
+            </div>
+            </div>
+        </div>
+
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-3">
+            <div class="card">
+            <div class="card-image">
+                <img src="/img/2.jpg"  alt="" class="responsive-img "/>
+                <span class="card-title">Stewed Beef Brisket with Tomato - $35</span>
+            </div>
+            </div>
+        </div>
+
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-3">
+            <div class="card">
+            <div class="card-image">
+                <img src="/img/3.jpg"  alt="" class="responsive-img "/>
+                <span class="card-title">Fried chicken chop - $23</span>
+            </div>
+            </div>
+        </div>
+
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-3">
+            <div class="card">
+            <div class="card-image">
+                <img src="/img/4.jpg"  alt="" class="responsive-img "/>
+                <span class="card-title">Boiled Meatballs - $24</span>
+            </div>
+            </div>
+        </div>
+        </div>     
+
+              <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-3">
+            <div class="card">
+            <div class="card-image">
+                <img src="/img/5.jpg"  alt="" class="responsive-img "/>
+                <span class="card-title">Spicy Shrimp Ball - $22</span>
+            </div>
+            </div>
+        </div>
+
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-3">
+            <div class="card">
+            <div class="card-image">
+                <img src="/img/6.jpg"  alt="" class="responsive-img "/>
+                <span class="card-title">Mapo Tofu - $19</span>
+            </div>
+            </div>
+        </div>
+
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-3">
+            <div class="card">
+            <div class="card-image">
+                <img src="/img/7.jpg"  alt="" class="responsive-img "/>
+                <span class="card-title">Korean army stew - $32</span>
+            </div>
+            </div>
+        </div>
+
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-3">
+            <div class="card">
+            <div class="card-image">
+                <img src="/img/8.jpg"  alt="" class="responsive-img "/>
+                <span class="card-title">Boiled fish - $27</span>
+            </div>
+            </div>
+        </div>
+        </div>     
+
+              <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-3">
+            <div class="card">
+            <div class="card-image">
+                <img src="/img/9.jpg"  alt="" class="responsive-img "/>
+                <span class="card-title">Candied sweet potatoes - $21</span>
+            </div>
+            </div>
+        </div>
+
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-3">
+            <div class="card">
+            <div class="card-image">
+                <img src="/img/10.jpg"  alt="" class="responsive-img "/>
+                <span class="card-title">Sweet and sour pork ribs - $32</span>
+            </div>
+            </div>
+        </div>
+
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-3">
+            <div class="card">
+            <div class="card-image">
+                <img src="/img/11.jpg"  alt="" class="responsive-img "/>
+                <span class="card-title">Braised pork trotters with soybeans - $28</span>
+            </div>
+            </div>
+        </div>
+
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-3">
+            <div class="card">
+            <div class="card-image">
+                <img src="/img/12.jpg"  alt="" class="responsive-img "/>
+                <span class="card-title">Boiled Prawns - $22</span>
+            </div>
+            </div>
+        </div>
+        </div>     
+
+              <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-3">
+            <div class="card">
+            <div class="card-image">
+                <img src="/img/13.jpg"  alt="" class="responsive-img "/>
+                <span class="card-title">Chicken stewed with mushrooms - $25</span>
+            </div>
+            </div>
+        </div>
+
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-3">
+            <div class="card">
+            <div class="card-image">
+                <img src="/img/14.jpg"  alt="" class="responsive-img "/>
+                <span class="card-title">Toothpick Beef - $29</span>
+            </div>
+            </div>
+        </div>
+
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-3">
+            <div class="card">
+            <div class="card-image">
+                <img src="/img/15.jpg"  alt="" class="responsive-img "/>
+                <span class="card-title">Red Bean Rice Cake - $12</span>
+            </div>
+            </div>
+        </div>
+
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-3">
+            <div class="card">
+            <div class="card-image">
+                <img src="/img/16.jpg"  alt="" class="responsive-img "/>
+                <span class="card-title">Enoki mushroom with garlic - $16</span>
+            </div>
+            </div>
+        </div>
+        </div>     
+
+              <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-3">
+            <div class="card">
+            <div class="card-image">
+                <img src="/img/17.jpg"  alt="" class="responsive-img "/>
+                <span class="card-title">Noodles with Soy Bean Paste - $15</span>
+            </div>
+            </div>
+        </div>
+
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-3">
+            <div class="card">
+            <div class="card-image">
+                <img src="/img/18.jpg"  alt="" class="responsive-img "/>
+                <span class="card-title">Pineapple Sweet and Sour Pork - $25</span>
+            </div>
+            </div>
+        </div>
+
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-3">
+            <div class="card">
+            <div class="card-image">
+                <img src="/img/19.jpg"  alt="" class="responsive-img "/>
+                <span class="card-title">Fried Pork in Scoop - $29</span>
+            </div>
+            </div>
+        </div>
+
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-3">
+            <div class="card">
+            <div class="card-image">
+                <img src="/img/20.jpg"  alt="" class="responsive-img "/>
+                <span class="card-title">Coke Chicken Wings - $21</span>
+            </div>
+            </div>
+        </div>
+        </div>     
+
+              <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-3">
+            <div class="card">
+            <div class="card-image">
+                <img src="/img/21.jpg"  alt="" class="responsive-img "/>
+                <span class="card-title">Fried tofu - $18</span>
+            </div>
+            </div>
+        </div>
+
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-3">
+            <div class="card">
+            <div class="card-image">
+                <img src="/img/22.jpg"  alt="" class="responsive-img "/>
+                <span class="card-title">Chicken curry - $20</span>
+            </div>
+            </div>
+        </div>
+
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-3">
+            <div class="card">
+            <div class="card-image">
+                <img src="/img/23.jpg"  alt="" class="responsive-img "/>
+                <span class="card-title">Spicy fried potato shreds - $14</span>
+            </div>
+            </div>
+        </div>
+
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-3">
+            <div class="card">
+            <div class="card-image">
+                <img src="/img/24.jpg"  alt="" class="responsive-img "/>
+                <span class="card-title">Yuxiang shredded pork - $24</span>
+            </div>
+            </div>
+        </div>
+        </div>     
+
+              <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-3">
+            <div class="card">
+            <div class="card-image">
+                <img src="/img/25.jpg"  alt="" class="responsive-img "/>
+                <span class="card-title">Kung Pao Diced Pork - $23</span>
+            </div>
+            </div>
+        </div>
+
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-3">
+            <div class="card">
+            <div class="card-image">
+                <img src="/img/26.jpg"  alt="" class="responsive-img "/>
+                <span class="card-title">Spicy Fried Clams - $26</span>
+            </div>
+            </div>
+        </div>
+
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-3">
+            <div class="card">
+            <div class="card-image">
+                <img src="/img/27.jpg"  alt="" class="responsive-img "/>
+                <span class="card-title">Shredded cabbage - $16</span>
+            </div>
+            </div>
+        </div>
+
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-3">
+            <div class="card">
+            <div class="card-image">
+                <img src="/img/28.jpg"  alt="" class="responsive-img "/>
+                <span class="card-title">Shredded Pork with Vegetables - $21</span>
+            </div>
+            </div>
+        </div>
+        </div>     
+
+              <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-3">
+            <div class="card">
+            <div class="card-image">
+                <img src="/img/29.jpg"  alt="" class="responsive-img "/>
+                <span class="card-title">Fried Larimichthys crocea - $33</span>
+            </div>
+            </div>
+        </div>
+
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-3">
+            <div class="card">
+            <div class="card-image">
+                <img src="/img/30.jpg"  alt="" class="responsive-img "/>
+                <span class="card-title">Ribs soup - $37</span>
+            </div>
+            </div>
+        </div>
+
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-3">
+            <div class="card">
+            <div class="card-image">
+                <img src="/img/31.jpg"  alt="" class="responsive-img "/>
+                <span class="card-title">Fried shrimp balls - $24</span>
+            </div>
+            </div>
+        </div>
+
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-3">
+            <div class="card">
+            <div class="card-image">
+                <img src="/img/32.jpg"  alt="" class="responsive-img "/>
+                <span class="card-title">Popcorn chicken - $22</span>
+            </div>
+            </div>
+        </div>
+        </div>     
+
+  
+
+</div>
+@endsection
