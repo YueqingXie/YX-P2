ce4344859aefc7a686640e64c931d0c80c214683
YueqingXie <43152856+YueqingXie@users.noreply.github.com>
2020-12-31 16:32:26 -0500

    Update menulayout.blade.php

--- resources/views/layouts/menulayout.blade.php
+++ resources/views/layouts/menulayout.blade.php
@@ -30,7 +30,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    <!-- {{ config('app.name', 'Laravel') }} -->
+                   
                     Y.X. P2
                 </a>
 
